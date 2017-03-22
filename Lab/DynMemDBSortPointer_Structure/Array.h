27940fd7e9f53f9ded89db74656c97a95b9a01c8
jemerson93 <jason.emerson93@gmail.com>
2017-03-22 13:48:48 -0700

    Signed-off-by: jemerson93 <jason.emerson93@gmail.com>

---
+++ Lab/DynMemDBSortPointer_Structure/Array.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   array.h
+ * Author: rcc
+ *
+ * Created on March 22, 2017, 12:55 PM
+ */
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+struct Array{
+    int size; //Size of Array
+    int *data; //Data in Array
+    int *index; //Index for Sorting
+};
+
+
+
+#endif /* ARRAY_H */
+
